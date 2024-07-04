7b57d5642db6ca6bbbeef47e2d3b5678ef7b47a3
Эстокадо Фамилио <mimimilka55@gmail.com>
2024-07-04 23:16:15 +0500

    server prod

--- index.ts
+++ index.ts
@@ -1,17 +1,18 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+const path = require("path");
+
 require("dotenv").config();
 
 const express = require("express");
+export const app = express();
 const sequelize = require("./db");
 const cors = require("cors");
 const router = require("./routes/index");
 const fileUpload = require("express-fileupload");
 const errorHandler = require("./middleware/errorHandlingMiddleware");
-import path from "path";
 
 const PORT = process.env.PORT || 5000;
 
-export const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, "static")));
